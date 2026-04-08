bda7988504461e9cc39b1d16cfaac4f3da563824
Maheswar Panda <pandamaheswar822@gmail.com>
2026-04-08 15:47:00 +0530

    twomock listofres

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,9 +2,45 @@ import Restaurantcard from "./Restaurantcard";
 import resList from "../utils/mockdata";
 
 const Body=()=>{
-    const ListofRestaurant=[
-
-    ]
+    const ListofRestaurant=[{
+        
+        
+                    "info": {
+                      "id": "196967",
+                      "name": "KFC",
+                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/1/8/4765a934-db3f-49ad-8242-8417952e0a3a_196967.JPG",
+                     
+                      "costForTwo": "₹450 for two",
+                      "cuisines": [
+                        "Burgers",
+                        "Fast Food",
+                        "Rolls & Wraps"
+                      ],
+                      "avgRating": 3.5,
+                      
+                    
+                     
+                    }
+                },{
+                    
+                    "info": {
+                      "id": "196367",
+                      "name": "Domino",
+                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/1/8/4765a934-db3f-49ad-8242-8417952e0a3a_196967.JPG",
+                     
+                      "costForTwo": "₹450 for two",
+                      "cuisines": [
+                        "Burgers",
+                        "Fast Food",
+                        "Rolls & Wraps"
+                      ],
+                      "avgRating": 4.5,
+                      
+                    
+                     
+                    }
+                }
+                    ]
     return (
         <div className="body">
             <div className="filter">
