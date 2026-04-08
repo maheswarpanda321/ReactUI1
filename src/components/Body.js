cac9a7a996d94c7c2467b00662515b6cf0f7ff12
Maheswar Panda <pandamaheswar822@gmail.com>
2026-04-08 18:36:05 +0530

    using api data

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,70 +4,7 @@ import { useState } from "react";
 
 const Body=()=>{
     // State Variable
-    let [ListofRestaurant,setListofRestaurant]=useState([
-        {
-
-    
-            "info": {
-                      "id": "196967",
-                      "name": "KFC",
-                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/1/8/4765a934-db3f-49ad-8242-8417952e0a3a_196967.JPG",
-                     
-                      "costForTwo": "₹450 for two",
-                      "cuisines": [
-                        "Burgers",
-                        "Fast Food",
-                        "Rolls & Wraps"
-                      ],
-                      "avgRating": 3.5,
-                      
-                    
-                     
-                    }
-        
-
-        }
-        ,{
-             
-                    
-                    "info": {
-                      "id": "196367",
-                      "name": "Domino",
-                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/1/8/4765a934-db3f-49ad-8242-8417952e0a3a_196967.JPG",
-                     
-                      "costForTwo": "₹450 for two",
-                      "cuisines": [
-                        "Burgers",
-                        "Fast Food",
-                        "Rolls & Wraps"
-                      ],
-                      "avgRating": 4.5,
-                      
-                    
-                     
-                    }
-                
-        },
-        {
-                    
-                    "info": {
-                      "id": "296367",
-                      "name": "Pizza Hot",
-                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/1/8/4765a934-db3f-49ad-8242-8417952e0a3a_196967.JPG",
-                     
-                      "costForTwo": "₹450 for two",
-                      "cuisines": [
-                        "Burgers",
-                        "Fast Food",
-                        "Rolls & Wraps"
-                      ],
-                      "avgRating": 4.3,
-                      
-                    
-                     
-                    }
-      }
-    ]);
+    let [ListofRestaurant,setListofRestaurant]=useState(resList);
 
 
 
